4323de0d4fefc9c1d7b83e43b19667d41bd752c5
Konstantin <149265315+FloppyAndOpticalDisksStorage@users.noreply.github.com>
2024-03-23 20:57:19 +0300

    Troubleshooting

--- blinkText.js
+++ blinkText.js
@@ -1,5 +1,5 @@
 setInterval(function() {
-    var el = document.getElementById("blinkingText");
+    var el = document.getElementById("blinkingtext");
     var colors = ['red','black'];
     el.style.color = colors[Math.floor(Math.random() * colors.length)];
 }, 1000);

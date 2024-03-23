2c9c1d1eed40a2c73429903d07d4c224090e8fb8
Konstantin <149265315+FloppyAndOpticalDisksStorage@users.noreply.github.com>
2024-03-23 22:23:55 +0300

    Make work calculator

---
+++ calcfunc.js
@@ -0,0 +1,6 @@
+var firstint = getElementById("first");
+var secondint = getElementById("second");
+var action = getElementById("action");
+var submit = getElementById("submit");
+var itog = getElementById("itog");
+

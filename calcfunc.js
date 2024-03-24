ea1568c0076d5c7281e1b3c5dbc0eadc1cce52c3
Konstantin <149265315+FloppyAndOpticalDisksStorage@users.noreply.github.com>
2024-03-24 13:17:04 +0300

    Troubleshooting

--- calcfunc.js
+++ calcfunc.js
@@ -4,3 +4,39 @@ var action = getElementById("action");
 var submit = getElementById("submit");
 var itog = getElementById("itog");
 
+// Define the actions
+const addAction = (num1, num2) => {
+  // Perform the add operation and update the result
+  const result = num1 + num2;
+  return result;
+}
+
+const subtractAction = (num1, num2) => {
+  // Perform the subtract operation and update the result
+  const result = num1 - num2;
+  return result;
+}
+
+const multiplyAction = (num1, num2) => {
+  // Perform the multiply operation and update the result
+  const result = num1 * num2;
+  return result;
+}
+
+const rasdelitAction = (num1, num2) => {
+  // Perform the multiply operation and update the result
+  const result = num1 / num2;
+  return result;
+}
+
+submit.addEventListener("submit",function(event) {
+  event.preventDefault();
+  if (action.value == "+") {
+    itog.innerHTML = "<b>" + parseString(addAction(parseFloat(firstint.value),parseFloat(secondint.value))) + "</b>"
+  } else if (action.value == "*") {
+    itog.innerHTML = "<b>" + parseString(multiplyAction(parseFloat(firstint.value),parseFloat(secondint.value))) + "</b>"
+  } else if (action.value == "-") {
+    itog.innerHTML = "<b>" + parseString(substractAction(parseFloat(firstint.value),parseFloat(secondint.value))) + "</b>"
+  } else if (action.value == "/" {
+    itog.innerHTML = "<b>" + parseString(rasdelitAction(parseFloat(firstint.value),parseFloat(secondint.value))) + "</b>"
+})
